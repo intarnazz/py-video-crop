fad08f9c8f480dc71089fa5516d3eb01d7c70cf9
Ortadox <misha.vidiakin@gmail.com>
2025-04-25 10:22:40 +0300

    const

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import os
 from moviepy.editor import VideoFileClip
 
-input_folder = './video'
-output_folder = './trimmed'
+START = 9
+END = 11
+
+input_folder = "./video"
+output_folder = "./trimmed"
 os.makedirs(output_folder, exist_ok=True)
 
 for filename in os.listdir(input_folder):
-    if filename.endswith('.mp4'):
+    if filename.endswith(".mp4"):
         input_path = os.path.join(input_folder, filename)
         output_path = os.path.join(output_folder, filename)
 
         with VideoFileClip(input_path) as clip:
             duration = clip.duration
-            start = 9
-            end = duration - 11
+            start = START
+            end = duration - END
             if end > start:  # только если видео достаточно длинное
                 trimmed = clip.subclip(start, end)
                 trimmed.write_videofile(output_path, codec="libx264")
