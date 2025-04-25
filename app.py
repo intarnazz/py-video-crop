f61b07f471371cc7246952f5f1b2d6825fec1843
Ortadox <misha.vidiakin@gmail.com>
2025-04-25 10:20:58 +0300

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from moviepy.editor import VideoFileClip
+
+input_folder = './video'
+output_folder = './trimmed'
+os.makedirs(output_folder, exist_ok=True)
+
+for filename in os.listdir(input_folder):
+    if filename.endswith('.mp4'):
+        input_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+
+        with VideoFileClip(input_path) as clip:
+            duration = clip.duration
+            start = 9
+            end = duration - 11
+            if end > start:  # только если видео достаточно длинное
+                trimmed = clip.subclip(start, end)
+                trimmed.write_videofile(output_path, codec="libx264")
+            else:
+                print(f"Слишком короткое видео: {filename}")
